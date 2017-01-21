a3f78e1638bab270f3203873dca7b0ff793f3202
Saurav Modak <saurav@linuxb.in>
2017-01-21 21:17:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello LiveEdu!')
+});
+ 
+app.listen(3000, () => {
+	console.log('App is running!');
+});
